ece4bbc1497e7a19a88664c17dd0d770648d13ab
Mariusz Gęzicki <mariusz.v7@gmail.com>
2017-04-08 11:51:44 +0200

    Initial commit

---
+++ src/main/java/pl/mrugames/commons/router/Router.java
@@ -0,0 +1,4 @@
+package pl.mrugames.commons.router;
+
+public class Router {
+}
---
+++ src/test/java/pl/mrugames/commons/router/RouterSpec.java
@@ -0,0 +1,8 @@
+package pl.mrugames.commons.router;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class RouterSpec {
+}
