26c73a00e331cf6821f93b632a9268e19252b26c
Mariusz Gęzicki <mariusz.v7@gmail.com>
2017-04-12 18:03:45 +0200

    Add implementation of Json resolver

--- src/main/java/pl/mrugames/commons/router/arg_resolvers/JsonPayloadArgumentResolver.java
+++ src/main/java/pl/mrugames/commons/router/arg_resolvers/JsonPayloadArgumentResolver.java
@@ -1,9 +1,15 @@
 package pl.mrugames.commons.router.arg_resolvers;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.stereotype.Component;
 import pl.mrugames.commons.router.RouteInfo;
+import pl.mrugames.commons.router.annotations.ArgDefaultValue;
+import pl.mrugames.commons.router.exceptions.IncompatibleParameterException;
+import pl.mrugames.commons.router.exceptions.ParameterNotFoundException;
+import pl.mrugames.commons.router.exceptions.RouterException;
 
 import java.io.IOException;
 import java.util.AbstractMap;
@@ -28,19 +34,34 @@ class JsonPayloadArgumentResolver implements PayloadArgumentResolver<String> {
                     .filter(p -> p.getParameterType() == RouteInfo.ParameterType.ARG)
                     .map(p -> map(p, rootNode))
                     .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        } catch (JsonParseException e) {
+            throw new RouterException(e.getMessage(), e);
         } catch (IOException e) {
-            e.printStackTrace(); // TODO: better exception handling
+            throw new RouterException(e.getMessage(), e);
         }
-        return null;
     }
 
     private Map.Entry<String, Object> map(RouteInfo.Parameter parameter, JsonNode root) {
         JsonNode node = root.get(parameter.getName());
-        Object mapped = null;
+
+        String strNode;
+        if (node == null) {
+            if (ArgDefaultValue.ARG_NULL_DEFAULT_VALUE.equals(parameter.getDefaultValue())) {
+                throw new ParameterNotFoundException(parameter.getName());
+            }
+
+            strNode = "\"" + parameter.getDefaultValue() + "\"";
+        } else {
+            strNode = node.toString();
+        }
+
+        Object mapped;
         try {
-            mapped = mapper.readValue(node.toString(), parameter.getType());
+            mapped = mapper.readValue(strNode, parameter.getType());
+        } catch (InvalidFormatException e) {
+            throw new IncompatibleParameterException(parameter.getName(), e.getTargetType());
         } catch (IOException e) {
-            e.printStackTrace(); /// TODO: better exception handling
+            throw new RouterException(e.getMessage(), e);
         }
 
         return new AbstractMap.SimpleEntry<>(parameter.getName(), mapped);
--- src/test/java/pl/mrugames/commons/router/arg_resolvers/JsonPayloadArgumentResolverSpec.java
+++ src/test/java/pl/mrugames/commons/router/arg_resolvers/JsonPayloadArgumentResolverSpec.java
@@ -2,7 +2,9 @@ package pl.mrugames.commons.router.arg_resolvers;
 
 import org.assertj.core.data.MapEntry;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -11,6 +13,9 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import pl.mrugames.commons.router.RouteInfo;
 import pl.mrugames.commons.router.RouterInitializer;
 import pl.mrugames.commons.router.TestConfiguration;
+import pl.mrugames.commons.router.exceptions.IncompatibleParameterException;
+import pl.mrugames.commons.router.exceptions.ParameterNotFoundException;
+import pl.mrugames.commons.router.exceptions.RouterException;
 
 import java.util.Map;
 
@@ -28,6 +33,9 @@ public class JsonPayloadArgumentResolverSpec {
     private RouterInitializer initializer;
 
     private Map<String, RouteInfo> routes;
+
+    @Rule
+    public final ExpectedException expectedException = ExpectedException.none();
 
     @Before
     public void before() {
@@ -59,4 +67,66 @@ public class JsonPayloadArgumentResolverSpec {
 
         assertThat(result).containsExactly(MapEntry.entry("description", "Test"));
     }
+
+    @Test
+    public void givenRequestDoesNotContainArgument_thenException() {
+        RouteInfo routeInfo = routes.get("POST:app/test/player/{playerId}");
+        String json = "{\"invalid\": \"Test\"}";
+
+        expectedException.expect(ParameterNotFoundException.class);
+        expectedException.expectMessage("Could not find 'description' parameter in the request");
+
+        resolver.resolve(json, routeInfo.getParameters());
+    }
+
+    @Test
+    public void givenRequestHasIncompatibleTypes_thenException() {
+        RouteInfo routeInfo = routes.get("GET:app/test/concat");
+        String json = "{\"a\": \"incompatible\", \"b\": \"str\", \"c\": 12.2, \"d\": \"xyz\"}";
+
+        expectedException.expect(IncompatibleParameterException.class);
+        expectedException.expectMessage("Could not convert 'a' into 'int'");
+
+        resolver.resolve(json, routeInfo.getParameters());
+    }
+
+    @Test
+    public void givenRequestDoesNotHaveOneParameter_thenDefaultValueShouldBeUsed() {
+        String json = "{\"a\": 1, \"b\": \"str\", \"c\": 12.2}";
+
+        RouteInfo routeInfo = routes.get("GET:app/test/concat");
+
+        Map<String, Object> result = resolver.resolve(json, routeInfo.getParameters());
+
+        assertThat(result).containsExactly(
+                MapEntry.entry("a", 1),
+                MapEntry.entry("b", "str"),
+                MapEntry.entry("c", 12.2),
+                MapEntry.entry("d", "last")
+        );
+    }
+
+    @Test
+    public void givenMethodHasNoArgAnnotations_thenReturnEmptyMap() {
+        String pattern = "GET:app/test/player/{playerId}";
+        RouteInfo routeInfo = routes.get(pattern);
+
+        String json = "";
+        Map<String, Object> result = resolver.resolve(json, routeInfo.getParameters());
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void givenInvalidJson_thenException() {
+        String pattern = "GET:app/test/player/{playerId}";
+        RouteInfo routeInfo = routes.get(pattern);
+
+        String json = "invalid";
+
+        expectedException.expect(RouterException.class);
+
+        resolver.resolve(json, routeInfo.getParameters());
+    }
+
 }
