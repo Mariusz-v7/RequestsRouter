954793dfdc2d7ec53668b3c34837db4b6d5d0520
Mariusz Gęzicki <mariusz.v7@gmail.com>
2017-08-13 20:34:00 +0200

    Add mapper config not to fail on unknown properties

--- src/main/java/pl/mrugames/commons/router/configuration/RouterConfiguration.java
+++ src/main/java/pl/mrugames/commons/router/configuration/RouterConfiguration.java
@@ -1,5 +1,6 @@
 package pl.mrugames.commons.router.configuration;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -27,6 +28,7 @@ class RouterConfiguration {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
         return mapper;
     }
